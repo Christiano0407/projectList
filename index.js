7dd856d7f9d8cc8a8aa9543bf3ea5985f0dfe7d2
Christiano0407 <placebo0704@gmail.com>
2022-04-12 07:39:15 -0500

    list add 02

---
+++ index.js
@@ -0,0 +1,11 @@
+//** ===>  Const or Variable === */
+const container = document.querySelector(".main__container");
+const submitBtn = document.querySelector("sub__btn");
+const input = document.querySelector(".f__input");
+const todos = document.querySelector(".todos");
+const todoContainer = document.querySelector(".todo__container");
+const todoForm = document.querySelector(".todo__form");
+const searchItem = document.querySelector(".todo-item");
+const searchInput = document.querySelector(".search__input");
+
+////  === Events ////
